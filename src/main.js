6a0789d3718ef2ae0619cd1765e3a81e8534dc73
taoguo <807081817@qq.com>
2022-11-11 16:17:22 +0800

    添加统计

--- src/main.js
+++ src/main.js
@@ -9,32 +9,30 @@ import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import './App.scss'
 
-
 Vue.config.productionTip = false
 
 Vue.use(VueI18n)
 Vue.use(ElementUI)
 
 const i18n = new VueI18n({
-  locale:'en',
-  messages
+  locale: 'en',
+  messages,
 })
-
 
 new Vue({
   i18n,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
 
 /**
  * todo
  * 添加语言切换功能
  * 删除语言功能
- * 
+ *
  * 界面改成左侧一个树，右边一个表格的形式，
  * 树用来管理节点
  * 表格用来管理语言和值
- * 
+ *
  * 底部文本域是否可以隐藏，提供预览按钮，节约界面空间。
- * 
+ *
  */
